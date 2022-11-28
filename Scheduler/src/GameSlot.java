bff96d910930de72f1a65cbd2895381771a73173
TheAlexTanasescu <alskiboy@yahoo.com>
2022-11-27 17:03:09 -0700

    first

--- Scheduler/src/GameSlot.java
+++ Scheduler/src/GameSlot.java
@@ -1,9 +1,24 @@
 
 public class GameSlot extends TimeSlot {
+    private int gameMax;
+	private int gameMin;
 
-	public GameSlot(String day, String time) {
+	public GameSlot(String day, String time, int gameMax, int gameMin) {
 		super.setDay(day);
 		super.setStartTime(time);
+		setGameMax(gameMax);
+		setGameMin(gameMin);
 	}
 
+	public void setGameMax(int gameMax) {
+		this.gameMax = gameMax;
+	}
+
+	public void setGameMin(int gameMin) {
+		this.gameMin = gameMin;
+	}
+
+	public String toString() {
+		return this.getDay() + " " + this.getStartTime() + " " + this.gameMax + " " + this.gameMin;
+	}
 }
